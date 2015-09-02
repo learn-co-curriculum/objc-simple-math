9aadfc9ee977508f7928bf93cf8227069305795b
markedwardmurray <markedwardmurray@gmail.com>
2015-09-02 10:53:46 -0400

    Finish Setup

--- objc-simple-math/FISAppDelegate.h
+++ objc-simple-math/FISAppDelegate.h
@@ -1,10 +1,4 @@
-//
 //  FISAppDelegate.h
-//  objc-simple-math
-//
-//  Created by iOS Staff on 9/2/15
-//  Copyright (c) 2015 The Flatiron School. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 
