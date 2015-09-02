09b7ac18ce98b28d533735a477aef3f1ffca3263
markedwardmurray <markedwardmurray@gmail.com>
2015-09-02 10:48:35 -0400

    Initial Commit

---
+++ objc-simple-math/FISAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FISAppDelegate.h
+//  objc-simple-math
+//
+//  Created by iOS Staff on 9/2/15
+//  Copyright (c) 2015 The Flatiron School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ objc-simple-math/Resources/Other-Sources/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  objc-simple-math
+//
+//  Created by iOS Staff on 9/2/15
+//  Copyright (c) 2015 The Flatiron School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FISAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FISAppDelegate class]));
+    }
+}
